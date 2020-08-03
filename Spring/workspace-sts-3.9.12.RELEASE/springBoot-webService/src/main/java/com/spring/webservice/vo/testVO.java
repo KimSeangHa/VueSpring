0dee362188da4c397cf7d88114f2dc74941cc96b
KimSeangHa <kshman1994@gmail.com>
2020-08-03 22:27:46 +0900

    Spring

---
+++ Spring/workspace-sts-3.9.12.RELEASE/springBoot-webService/src/main/java/com/spring/webservice/vo/testVO.java
@@ -0,0 +1,19 @@
+package com.spring.webservice.vo;
+
+public class testVO {
+	private int member_num;
+	private String member_name;
+	
+	public int getMember_num() {
+		return member_num;
+	}
+	public void setMember_num(int member_num) {
+		this.member_num = member_num;
+	}
+	public String getMember_name() {
+		return member_name;
+	}
+	public void setMember_name(String member_name) {
+		this.member_name = member_name;
+	}
+}
