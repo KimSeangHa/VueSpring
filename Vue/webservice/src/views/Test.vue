82f1c7971cefbc67d0a6e2108c6fa81ea0b14423
KimSeangHa <kshman1994@gmail.com>
2020-08-03 22:30:00 +0900

    Vue

---
+++ Vue/webservice/src/views/Test.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+    <h1>TESaaaaaaT</h1><br>
+    <button @click="getData">DB 테스트</button>
+    <br>
+    <button class="btn btn-primary">aaa</button>
+  </div>
+</template>
+
+<script>
+export default {
+    methods: {
+        getData () {
+            this.$http.get('http://localhost:9090/webservice/dbtest', {
+            })
+            .then((result) => {
+                console.log(result)
+            })
+            .catch(e => {
+                console.log(e)
+            })
+        }
+    },
+    mounted () {
+       // alert()
+    }
+}
+</script>
